48a14797ddab5f128a766815f8b786143614f118
RowanFerrabee <rowan.ferrabee@gmail.com>
2015-11-17 21:47:13 -0500

    Fuck this pls take over my computer is dying

--- GeneticMain/Match.cpp
+++ GeneticMain/Match.cpp
@@ -2,7 +2,11 @@
 #include "globals.cpp"
 
 Match::Match() {
-    matchData = new int[CELLS];
+}
+
+Match::Match(AbstractPlayer* p1, AbstractPlayer* p2) {
+    player1 = p1;
+    player2 = p2;
 }
 
 Match::~Match() {
@@ -19,15 +23,47 @@ void Match::setPlayer2(AbstractPlayer* p) {
     player2 = p;
 }
 
-int Match::playOut() {
-    return 0;
-}
-int Match::checkWinConditions() {
-    for(int i = 0; i < CELLS; i++) {
+int Match::playOut() { //Runs a match and returns the ID of the winner, 0 is a draw
+    bool gameOver = false;
+    int move = 0, winningPlayer = 0;
+    do {
+        if (move%2) {
+            column = player1.makeMove(matchData);
+        } else {
+            column = player2.makeMove(matchData);
+        }
+        applyMove(column);
+        gameOver = checkWinConditions(column);
+        move++;
+    } while(!gameOver && move < ROWS*COLUMNS);
 
-
+    if (move == ROWS*COLUMNS)
+        return DRAW;
+    else {
+        if (move%2)
+            return player2.getId();
+        else
+            return player1.getId();
     }
-    return 0;
+}
+int Match::checkWinConditions(int column) {
+    int row = 6;
+    while(row >= 0) {
+        if (matchData[row][column] != 0) break;
+        row--;
+    } if (row == -1) {
+        return ERROR;
+    }
+
+    if (
+        checkForRowWin(row,column) ||
+        checkForColumnWin(row,column) ||
+        checkForDiagonalWin(row,column)
+    ) {
+        return true
+    }
+    
+    return false;
 }
 
 bool Match::checkForRowWin(int cell) {
