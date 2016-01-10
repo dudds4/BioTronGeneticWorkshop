b47d2b5d45b9141f120a1cd81cb6b30271dcee92
RowanFerrabee <rowan.ferrabee@gmail.com>
2016-01-10 12:59:54 -0500

    implemented tic-tac-toe player

--- GeneticMain/Player.cpp
+++ GeneticMain/Player.cpp
@@ -3,36 +3,290 @@
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <fstream>
+#include <cmath>
+
+Player::Neuron::Neuron() {
+	in = 42;
+	out = 7;
+	inputWeights = new int[in];
+	outputWeights = new int[out];
+	for (int i = 0; i < in; i++) {
+		inputWeights[i] = rand()%50;
+	}
+	for (int i = 0; i < out; i++) {
+		outputWeights[i] = rand()%50;
+	}
+}
+
+Player::Neuron::Neuron(int _in, int _out) {
+	in = _in;
+	out = _out;
+	inputWeights = new int[in];
+	outputWeights = new int[out];
+	for (int i = 0; i < in; i++) {
+		inputWeights[i] = rand()%50;
+	}
+	for (int i = 0; i < out; i++) {
+		outputWeights[i] = rand()%50;
+	}
+}
+
+void Player::Neuron::mutate() {
+	for (int i = 0; i < in; i++) {
+		inputWeights[i] += (2*(rand()%2)-1)*(10-(int)pow(rand()%100,1/2));
+		if (inputWeights[i] < 0) {
+			inputWeights[i] = 0;
+		} else if (inputWeights[i] > 50) {
+			inputWeights[i] = 50;
+		}
+	}
+	for (int i = 0; i < out; i++) {
+		outputWeights[i] += (2*(rand()%2)-1)*(10-(int)pow(rand()%100,1/2));
+		if (outputWeights[i] < 0) {
+			outputWeights[i] = 0;
+		} else if (outputWeights[i] > 50) {
+			outputWeights[i] = 50;
+		}
+	}
+}
+
+Player::Brain::Brain() {
+	size = neuronCount;
+}
+
+Player::Brain::Brain(int _size) {
+	size = _size;
+}
+
+void Player::Brain::init(int _in, int _out, int _size) {
+	in = _in;
+	out = _out;
+	size = _size;
+	neurons = new Neuron[size];
+	for (int i=0; i<size; i++) {
+		neurons[i] = Neuron(in, out);
+	}
+}
+
+void Player::Brain::execute(int input[], int inputSize, int output[], int outputSize) {
+	if (outputSize != neurons[0].out || inputSize != neurons[0].in) {
+		std::cout << "Wrong size, quitting\n";
+		return;
+	}// std::cout << "Executing\n";
+	for (int i=0; i<outputSize; i++) {
+		output[i] = 0;
+	}
+	for (int i=0; i<size; i++) {
+		neurons[i].value = 0;
+		for (int j=0; j<inputSize; j++) {
+			neurons[i].value += 10*input[j]*neurons[i].inputWeights[j]+neurons[i].inputWeights[j];
+		}
+		for (int j=0; j<outputSize; j++) {
+			output[j] += neurons[i].outputWeights[j]*neurons[i].value;
+		}
+	}
+}
 
 Player::Player() {
 	time_t timer;
 	srand(time(&timer));
 }
 
+Player::Player(bool init) {
+	if (init) {
+		brain1.init(9,15,100);
+		brain2.init(9,15,100);
+		brain3.init(15,9,80);
+	}
+}
+
 Player* Player::copy() {
+//	std::cout << "Copy\n";
 	Player* newPlayer = new Player();
 	*(newPlayer) = *(this);
 	return newPlayer;
 }
 
 Player* Player::mutate() {
-	return new Player();
+//	std::cout << "Mutate\n";
+	int random;
+	Player* player = new Player();
+	*player = *this;
+	for (int i=0; i<brain1.size/8; i++) {
+		random = rand()%brain1.size;
+		if (i%2 == 0) {
+			player->brain1.neurons[random].mutate();
+		} else {
+			player->brain2.neurons[random].mutate();
+		}
+	}
+	for (int i=0; i<brain3.size/16; i++) {
+		random = rand()%brain3.size;
+		player->brain3.neurons[random].mutate();
+	}
+	return player;
 }
 
-Player* Player::mate(Player*) {
-	return new Player();
+Player* Player::mate(Player* parent) {
+//	std::cout << "Mate\n";
+	Player* child = new Player();
+	*(child) = *(this);
+	for (int i=0; i<brain1.size; i++) {
+		if (rand()%2 == 0) {
+			child->brain1.neurons[i] = parent->brain1.neurons[i];
+		}
+		if (rand()%2 == 0) {
+			child->brain2.neurons[i] = parent->brain2.neurons[i];
+		}
+	}
+	for (int i=0; i<brain3.size; i++) {
+		if (rand()%2 == 0) {
+			child->brain3.neurons[i] = parent->brain3.neurons[i];
+		}
+	}
+	return child;
 }
 Player* Player::random() {
-	return new Player();
-}
-
-Player* Player::fromFile(std::string) {
-	return new Player();
-}
-void Player::toFile(std::string) {
-
+//	std::cout << "Random\n";
+	return new Player(true);
 }
 
 int Player::makeMove(int board[][3], int player) {
-	return rand()%9;
+	int positions[9];
+	int midSize = 15;
+
+	//Brain1
+	int p1Board[9], p1Output[midSize];
+	for (int i=0; i<3; i++) {
+		for (int j=0; j<3; j++) {
+			if (board[i][j] == player) {
+				p1Board[3*i+j]=1;
+			} else {
+				p1Board[3*i+j]=0;
+			}
+		}
+	}
+	brain1.execute(p1Board, 9, p1Output, midSize);
+
+	//Brain2
+	int p2Board[9], p2Output[midSize];
+	for (int i=0; i<3; i++) {
+		for (int j=0; j<3; j++) {
+			if (board[i][j] != player && board[i][j] != 0) {
+				p2Board[3*i+j]=1;
+			} else {
+				p2Board[3*i+j]=0;
+			}
+		}
+	}
+	brain2.execute(p2Board, 9, p2Output, midSize);
+
+	int p3Board[midSize];
+	for (int i=0; i<midSize; i++) {
+		p3Board[i] = p1Output[i] + p2Output[i];
+	}
+	brain3.execute(p3Board, midSize, positions, 9);
+
+	int position = 0;
+	for (int i=1; i<9; i++) {
+		if (positions[i] > positions[position]) {
+			position = i;
+		}
+	}
+	return position;
+}
+
+Player* Player::fromFile(std::string fileName) {
+	Player* newPlayer = new Player(true);
+	std::ifstream file;
+	file.open(fileName);
+	int input_size, b1, b2, mid_size, b3, output_size;
+	file >> input_size;
+	file >> b1;
+	file >> b1;
+	file >> mid_size;
+	file >> b3;
+	file >> output_size;
+	if (input_size != 9 || output_size != 9) {
+		file.close();
+		delete newPlayer;
+		return Player::random();
+	}
+
+	int input_weight, output_weight;
+	for (int i=0; i<newPlayer->brain1.size; i++) {
+		for (int j=0; j<input_size; j++) {
+			file >> input_weight;
+			newPlayer->brain1.neurons[i].inputWeights[j] = input_weight;
+		}
+		for (int j=0; j<mid_size; j++) {
+			file >> output_weight;
+			newPlayer->brain1.neurons[i].outputWeights[j] = output_weight;
+		}
+	}
+
+	for (int i=0; i<newPlayer->brain2.size; i++) {
+		for (int j=0; j<input_size; j++) {
+			file >> input_weight;
+			newPlayer->brain2.neurons[i].inputWeights[j] = input_weight;
+		}
+		for (int j=0; j<mid_size; j++) {
+			file >> output_weight;
+			newPlayer->brain2.neurons[i].outputWeights[j] = output_weight;
+		}
+	}
+
+	for (int i=0; i<newPlayer->brain3.size; i++) {
+		for (int j=0; j<mid_size; j++) {
+			file >> input_weight;
+			newPlayer->brain3.neurons[i].inputWeights[j] = input_weight;
+		}
+		for (int j=0; j<output_size; j++) {
+			file >> output_weight;
+			newPlayer->brain3.neurons[i].outputWeights[j] = output_weight;
+		}
+	}
+	file.close();
+	return newPlayer;
+}
+
+void Player::toFile(std::string fileName) {
+	std::ofstream file;
+	file.open(fileName);
+	int midSize = 15;
+	file << 9 << std::endl;
+	file << brain1.size << std::endl;
+	file << brain2.size << std::endl;
+	file << midSize << std::endl;
+	file << brain3.size << std::endl;
+	file << 9 << std::endl;
+
+	for (int i=0; i<brain1.size; i++) {
+		for (int j=0; j<9; j++) {
+			file << brain1.neurons[i].inputWeights[j] << std::endl;
+		}
+		for (int j=0; j<midSize; j++) {
+			file << brain1.neurons[i].outputWeights[j] << std::endl;
+		}
+	}
+
+	for (int i=0; i<brain2.size; i++) {
+		for (int j=0; j<9; j++) {
+			file << brain2.neurons[i].inputWeights[j] << std::endl;
+		}
+		for (int j=0; j<midSize; j++) {
+			file << brain2.neurons[i].outputWeights[j] << std::endl;
+		}
+	}
+
+	for (int i=0; i<brain3.size; i++) {
+		for (int j=0; j<midSize; j++) {
+			file << brain3.neurons[i].inputWeights[j] << std::endl;
+		}
+		for (int j=0; j<9; j++) {
+			file << brain3.neurons[i].outputWeights[j] << std::endl;
+		}
+	}
+	file.close();
 }
--- GeneticMain/Player.h
+++ GeneticMain/Player.h
@@ -3,12 +3,35 @@
 
 #include "AbstractPlayer.h"
 
+#define neuronCount 200
+
 class Player : public AbstractPlayer {
 	int skill;
+	class Neuron {
+	public:
+		Neuron();
+		Neuron(int, int);
+		void mutate();
+		int value;
+		int in, out;
+		int* outputWeights;
+		int* inputWeights;
+	};
+	class Brain {
+	public:
+		Brain();
+		Brain(int _size);
+		int in, out, size;
+		void init(int, int, int);
+		void execute(int in[], int, int out[], int);
+		Neuron *neurons;
+	};
 public:
 	Player();
+	Player(bool init);
 
 	double fitness;
+	Brain brain1, brain2, brain3;
 	
 	Player* mutate();
 	Player* copy();
