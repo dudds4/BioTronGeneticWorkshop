980d8896f2611723ac06f662527f195d3a8a0648
RowanFerrabee <rowan.ferrabee@gmail.com>
2016-01-11 13:51:07 -0500

    added comments

--- GeneticMain/Player.cpp
+++ GeneticMain/Player.cpp
@@ -7,6 +7,7 @@
 Player::Player() {
 	time_t timer;
 	srand(time(&timer));
+//  Instantiates a new Player
 }
 
 Player* Player::copy() {
@@ -16,23 +17,32 @@ Player* Player::copy() {
 }
 
 Player* Player::mutate() {
+//  Returns a copy of current player
+//  with minor random differences.
 	return new Player();
 }
 
-Player* Player::mate(Player*) {
+Player* Player::mate(Player* p2) {
+//  Returns a new player whose data
+//	structure mixes the data from p2 and
+//  the current player
 	return new Player();
 }
 Player* Player::random() {
+//	Returns a player with entirely randomized data
 	return new Player();
 }
 
-Player* Player::fromFile(std::string) {
+Player* Player::fromFile(std::string fileName) {
+//  Returns a player whose data structure is outlined in a file
 	return new Player();
 }
-void Player::toFile(std::string) {
-
+void Player::toFile(std::string fileName) {
+//  Stores the player's data into a file
 }
 
 int Player::makeMove(int board[][3], int player) {
+//  Given a board and the current player's ID,
+//  returns a position where the Player would like to play.
 	return rand()%9;
 }
--- GeneticMain/Player.h
+++ GeneticMain/Player.h
@@ -5,6 +5,9 @@
 
 class Player : public AbstractPlayer {
 	int skill;
+	/*
+	Place Data Structure Here
+	*/
 public:
 	Player();
 
